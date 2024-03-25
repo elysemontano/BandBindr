df4443b6e0a7888b7a62e61e6a2a23d4cc74451f
Elyse Montano <elysemontano.dev@gmail.com>
2024-03-25 13:58:38 -0700

    org members index

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,11 @@
 class MembersController < ApplicationController
+    before_action :set_current_organization
+    def index
+        @members = @current_organization.members
+    end
+
+    private
+    def set_current_organization
+        @current_organization = Organization.find(params[:organization_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :members
-  resources :organizations
+  # Members are scoped through organizations:
+  resources :organizations do
+    resources :members
+  end
   get 'static_pages/landing_page'
   root 'static_pages#landing_page'
   devise_for :users
