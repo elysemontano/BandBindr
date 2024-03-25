8bfa1bd8d16249a740ff765367d59a15a0fe22c6
Elyse Montano <elysemontano.dev@gmail.com>
2024-03-25 13:15:17 -0700

    adds member resource and associations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
 class Organization < ApplicationRecord
     validates :name, presence: true
+    has_many :members 
+    has_many :users, through: :members
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_25_200521) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_25_201204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "members", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "organization_id", null: false
+    t.jsonb "roles", default: {}, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organization_id"], name: "index_members_on_organization_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string "name"
@@ -32,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_25_200521) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "members", "organizations"
+  add_foreign_key "members", "users"
 end
