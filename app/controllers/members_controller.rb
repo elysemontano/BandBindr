f646e95824d51763307645810ab2c121b953040d
Elyse Montano <elysemontano.dev@gmail.com>
2024-03-25 14:24:18 -0700

    invite to become member of organizatioN

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,6 +4,15 @@ class MembersController < ApplicationController
         @members = @current_organization.members
     end
 
+    def invite 
+        email = params[:email]
+        return redirect_to organization_members_path(@current_organization), alert: 'No email provided' if email.blank?
+        user = User.find_by(email: email) || User.invite!({ email: email}, current_user)
+        return redirect_to organization_members_path(@current_organization), alert: 'Email invalid' unless user.valid?
+        user.members.find_or_create_by(organization: @current_organization, roles: {admin: false, editor: true})
+        redirect_to organization_members_path(@current_organization), notice: "#{email} invited"
+    end
+
     private
     def set_current_organization
         @current_organization = Organization.find(params[:organization_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # Members are scoped through organizations:
   resources :organizations do
-    resources :members
+    resources :members do 
+      # allows member to be invited:
+      collection do 
+        post :invite
+      end
+    end
   end
   get 'static_pages/landing_page'
   root 'static_pages#landing_page'
