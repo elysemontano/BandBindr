653abe892e1a8a7e2cf8b540291e7ebcae9ff4b6
Elyse Montano <elysemontano.dev@gmail.com>
2024-03-27 14:11:46 -0700

    adds edit and delete functionality with flash message when deleting

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -16,7 +16,26 @@ class PeopleController < AuthorizedController
         @person = @current_organization.people.create(person_params)
         if @person.valid?
             redirect_to organization_people_path(@current_organization)
-          end
+        end
+    end
+
+    def edit
+        @person = @current_organization.people.find(params[:id])
+    end
+
+    def update 
+        @person = @current_organization.people.find(params[:id])
+        @person.update(person_params)
+        if @person.valid?
+            redirect_to organization_people_path(@current_organization)
+      end
+    end
+
+    def destroy
+        @person = @current_organization.people.find(params[:id])
+        if @person.destroy
+            redirect_to organization_people_path(@current_organization)
+        end
     end
 
     private
