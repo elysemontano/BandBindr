8692b542ce87a093f26be83a26c8ef1b37f2ae3a
Elyse Montano <elysemontano.dev@gmail.com>
2024-03-27 15:05:20 -0700

    adds tag functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,45 @@
-class TagsController < ApplicationController
+class TagsController < AuthorizedController
+    def index
+        @tags = @current_organization.tags
+    end
+
+    def show
+        @tag = @current_organization.tags.find(params[:id])
+        @songs = @tag.song_tags.includes(:song)
+    end
+
+    def new
+        @tag = Tag.new
+    end
+
+    def create
+        @tag = @current_organization.tags.create(tag_params)
+        if @tag.valid?
+            redirect_to organization_tags_path(@current_organization)
+        end
+    end
+
+    def edit
+        @tag = @current_organization.tags.find(params[:id])
+    end
+
+    def update 
+        @tag = @current_organization.tags.find(params[:id])
+        @tag.update(tag_params)
+        if @tag.valid?
+            redirect_to organization_tags_path(@current_organization)
+      end
+    end
+
+    def destroy
+        @tag = @current_organization.tags.find(params[:id])
+        if @tag.destroy
+            redirect_to organization_tags_path(@current_organization)
+        end
+    end
+
+    private
+    def tag_params
+        params.require(:tag).permit(:tag_word, :organization_id)
+    end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   validates :organization_id, :tag_word, presence: true
+  validates :tag_word, uniqueness: true
   belongs_to :organization
   has_many :songs, through: :song_tags
   has_many :song_tags
