8e8cec39acc2565056f616717bec69b1fe6a31c7
Elyse Montano <elysemontano.dev@gmail.com>
2024-03-27 11:02:54 -0700

    adds create functionality for people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,4 +7,20 @@ class PeopleController < AuthorizedController
         @person = @current_organization.people.find(params[:id])
         @songs = @person.keys.includes(:song)
     end
+
+    def new
+        @person = Person.new
+    end
+
+    def create
+        @person = @current_organization.people.create(person_params)
+        if @person.valid?
+            redirect_to organization_people_path(@current_organization)
+          end
+    end
+
+    private
+    def person_params
+        params.require(:person).permit(:first_name, :last_name, :organization_id)
+    end
 end
