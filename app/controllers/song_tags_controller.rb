a06ee3037720ae8c2d2eee545a5436f7c2d04ffa
Elyse Montano <elysemontano.dev@gmail.com>
2024-04-04 11:35:50 -0700

    adds styling to tags

--- app/controllers/song_tags_controller.rb
+++ app/controllers/song_tags_controller.rb
@@ -17,12 +17,19 @@ class SongTagsController < SongsController
         @song_tag = SongTag.find(params[:id])
         @song = @current_organization.songs.find(params[:song_id])
         if @song_tag.destroy
-            redirect_to organization_song_path(@current_organization, @song)
+            redirect_to stored_last_visited_show_page || root_path
+        else
+            flash[:alert] = 'Failed to delete tag.'
+            redirect_back(fallback_location: root_path)
         end
     end
 
     private 
     def song_tag_params 
         params.require(:song_tag).permit(:song_id, :tag_id)
+    end
+
+    def stored_last_visited_show_page
+        session[:last_visited_show_page]
     end
 end
